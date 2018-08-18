7bafce2647f8f29f1af5c90eb81d8ce0c4c6256f
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-08-18 22:35:57 +0530

    updated response type class

--- core/src/main/java/com/vishnu/aggarwal/core/enums/RestApiEndPoint.java
+++ core/src/main/java/com/vishnu/aggarwal/core/enums/RestApiEndPoint.java
@@ -6,12 +6,17 @@ Created by vishnu on 9/3/18 11:08 AM
 
 import com.vishnu.aggarwal.core.constants.UrlMapping.Rest.Quartz;
 import com.vishnu.aggarwal.core.constants.UrlMapping.Rest.User;
+import com.vishnu.aggarwal.core.dto.UserAuthenticationDTO;
+import com.vishnu.aggarwal.core.dto.UserDTO;
+import com.vishnu.aggarwal.core.vo.DataTableVO;
 import com.vishnu.aggarwal.core.vo.RestResponseVO;
 import lombok.Getter;
+import org.springframework.http.HttpMethod;
+
+import java.util.Map;
 
 import static com.vishnu.aggarwal.core.constants.UrlMapping.Rest.User.*;
 import static java.lang.String.format;
-import static java.util.Arrays.asList;
 import static org.springframework.http.HttpMethod.*;
 
 /**
@@ -23,7 +28,7 @@ public enum RestApiEndPoint {
     /**
      * Create new job quartz rest api end point enum.
      */
-    CREATE_NEW_JOB(getQuartzRestUrlEndPoint(Quartz.CREATE_JOB), POST, RestResponseVO.class),
+    CREATE_NEW_JOB(getQuartzRestUrlEndPoint(Quartz.CREATE_JOB), POST, Map.class),
 
     /**
      * Create new trigger quartz rest api end point enum.
@@ -33,72 +38,72 @@ public enum RestApiEndPoint {
     /**
      * Update existing job rest api end point.
      */
-    UPDATE_EXISTING_JOB(getQuartzRestUrlEndPoint(Quartz.UPDATE_TRIGGER), asList(PUT, PATCH), RestResponseVO.class),
+    UPDATE_EXISTING_JOB(getQuartzRestUrlEndPoint(Quartz.UPDATE_TRIGGER), PUT, RestResponseVO.class),
 
     /**
      * Update existing trigger rest api end point.
      */
-    UPDATE_EXISTING_TRIGGER(getQuartzRestUrlEndPoint(Quartz.UPDATE_TRIGGER), asList(PUT, PATCH), RestResponseVO.class),
+    UPDATE_EXISTING_TRIGGER(getQuartzRestUrlEndPoint(Quartz.UPDATE_TRIGGER), PUT, RestResponseVO.class),
 
     /**
      * Fetch jobs by group name quartz rest api end point enum.
      */
-    FETCH_JOBS_BY_GROUP_NAME(getQuartzRestUrlEndPoint(Quartz.FETCH_JOB_BY_GROUP_NAME), GET, RestResponseVO.class),
+    FETCH_JOBS_BY_GROUP_NAME(getQuartzRestUrlEndPoint(Quartz.FETCH_JOB_BY_JOB_GROUP_NAME), GET, DataTableVO.class),
 
     /**
      * Fetch triggers by job key name and group name quartz rest api end point enum.
      */
-    FETCH_TRIGGERS_BY_JOB_KEY_NAME_AND_GROUP_NAME(getQuartzRestUrlEndPoint(Quartz.FETCH_TRIGGER_BY_JOB_KEY_GROUP_NAME), GET, RestResponseVO.class),
+    FETCH_TRIGGERS_BY_JOB_KEY_NAME_AND_GROUP_NAME(getQuartzRestUrlEndPoint(Quartz.FETCH_TRIGGER_BY_JOB_KEY_JOB_GROUP_NAME), GET, DataTableVO.class),
 
     /**
      * Fetch quartz details by group name quartz rest api end point enum.
      */
-    FETCH_QUARTZ_DETAILS_BY_GROUP_NAME(getQuartzRestUrlEndPoint(Quartz.FETCH_QUARTZ_DETAILS_GROUP_NAME), GET, RestResponseVO.class),
+    FETCH_QUARTZ_DETAILS_BY_GROUP_NAME(getQuartzRestUrlEndPoint(Quartz.FETCH_QUARTZ_DETAILS_JOB_GROUP_NAME), GET, DataTableVO.class),
 
     /**
      * Resume jobs quartz rest api end point enum.
      */
-    RESUME_JOBS(getQuartzRestUrlEndPoint(Quartz.RESUME_JOBS), asList(PUT, PATCH), RestResponseVO.class),
+    RESUME_JOBS(getQuartzRestUrlEndPoint(Quartz.RESUME_JOBS), PUT, Map.class),
 
     /**
      * Pause jobs quartz rest api end point enum.
      */
-    PAUSE_JOBS(getQuartzRestUrlEndPoint(Quartz.PAUSE_JOBS), asList(PUT, PATCH), RestResponseVO.class),
+    PAUSE_JOBS(getQuartzRestUrlEndPoint(Quartz.PAUSE_JOBS), PUT, Map.class),
 
     /**
      * Resume triggers quartz rest api end point enum.
      */
-    RESUME_TRIGGERS(getQuartzRestUrlEndPoint(Quartz.RESUME_TRIGGERS), asList(PUT, PATCH), RestResponseVO.class),
+    RESUME_TRIGGERS(getQuartzRestUrlEndPoint(Quartz.RESUME_TRIGGERS), PUT, Map.class),
 
     /**
      * Pause triggers quartz rest api end point enum.
      */
-    PAUSE_TRIGGERS(getQuartzRestUrlEndPoint(Quartz.PAUSE_TRIGGERS), asList(PUT, PATCH), RestResponseVO.class),
+    PAUSE_TRIGGERS(getQuartzRestUrlEndPoint(Quartz.PAUSE_TRIGGERS), PUT, Map.class),
 
     /**
      * Delete jobs quartz rest api end point enum.
      */
-    DELETE_JOBS(getQuartzRestUrlEndPoint(Quartz.DELETE_JOBS), DELETE, RestResponseVO.class),
+    DELETE_JOBS(getQuartzRestUrlEndPoint(Quartz.DELETE_JOBS), DELETE, Map.class),
 
     /**
      * Delete triggers quartz rest api end point enum.
      */
-    DELETE_TRIGGERS(getQuartzRestUrlEndPoint(Quartz.DELETE_TRIGGERS), DELETE, RestResponseVO.class),
+    DELETE_TRIGGERS(getQuartzRestUrlEndPoint(Quartz.DELETE_TRIGGERS), DELETE, Map.class),
 
     /**
      * User authentication rest api end point.
      */
-    USER_AUTHENTICATION(getUserRestUrlEndPoint(AUTHENTICATE), GET, RestResponseVO.class),
+    USER_AUTHENTICATION(getUserRestUrlEndPoint(AUTHENTICATE), GET, UserAuthenticationDTO.class),
 
     /**
      * Current logged in user rest api end point.
      */
-    CURRENT_LOGGED_IN_USER(getUserRestUrlEndPoint(User.CURRENT_LOGGED_IN_USER), GET, RestResponseVO.class),
+    CURRENT_LOGGED_IN_USER(getUserRestUrlEndPoint(User.CURRENT_LOGGED_IN_USER), GET, UserDTO.class),
 
     /**
      * Login user rest api end point.
      */
-    LOGIN_USER(getUserRestUrlEndPoint(LOGIN), POST, RestResponseVO.class),
+    LOGIN_USER(getUserRestUrlEndPoint(LOGIN), POST, UserAuthenticationDTO.class),
 
     /**
      * Logout user rest api end point.
@@ -106,12 +111,12 @@ public enum RestApiEndPoint {
     LOGOUT_USER(getUserRestUrlEndPoint(LOGOUT), POST, RestResponseVO.class);
 
     private String apiEndPoint;
-    private Object httpMethods;
-    private Class<?> responseTypeClass;
+    private HttpMethod httpMethod;
+    private Class responseTypeClass;
 
-    RestApiEndPoint(String apiEndPoint, Object httpMethods, Class<?> responseTypeClass) {
+    RestApiEndPoint(String apiEndPoint, HttpMethod httpMethod, Class responseTypeClass) {
         this.apiEndPoint = apiEndPoint;
-        this.httpMethods = httpMethods;
+        this.httpMethod = httpMethod;
         this.responseTypeClass = responseTypeClass;
     }
 
