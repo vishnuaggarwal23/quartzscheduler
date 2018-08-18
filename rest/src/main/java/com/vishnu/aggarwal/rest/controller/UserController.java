e9dd128b0bc2e8127cae767b892d1751f2ea0173
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-08-18 22:24:56 +0530

    added only the fetch current logged in user api

--- rest/src/main/java/com/vishnu/aggarwal/rest/controller/UserController.java
+++ rest/src/main/java/com/vishnu/aggarwal/rest/controller/UserController.java
@@ -2,27 +2,27 @@ package com.vishnu.aggarwal.rest.controller;
 
 import com.vishnu.aggarwal.core.controller.BaseController;
 import com.vishnu.aggarwal.core.dto.UserDTO;
-import com.vishnu.aggarwal.core.vo.RestResponseVO;
 import com.vishnu.aggarwal.rest.service.UserService;
 import lombok.extern.apachecommons.CommonsLog;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static com.vishnu.aggarwal.core.constants.ApplicationConstants.X_AUTH_TOKEN;
-import static com.vishnu.aggarwal.core.constants.UrlMapping.Rest.User.*;
-import static java.lang.Boolean.FALSE;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static com.vishnu.aggarwal.core.constants.ApplicationConstants.CUSTOM_REQUEST_ID;
+import static com.vishnu.aggarwal.core.constants.UrlMapping.Rest.User.BASE_URI;
+import static com.vishnu.aggarwal.core.constants.UrlMapping.Rest.User.CURRENT_LOGGED_IN_USER;
+import static com.vishnu.aggarwal.rest.util.DTOConversion.convertFromUser;
+import static java.text.MessageFormat.format;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace;
 import static org.springframework.http.HttpStatus.valueOf;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /*
 Created by vishnu on 30/3/18 10:46 AM
@@ -34,52 +34,32 @@ Created by vishnu on 30/3/18 10:46 AM
 @RestController
 @CommonsLog
 @RequestMapping(value = BASE_URI, produces = APPLICATION_JSON_UTF8_VALUE)
+@Secured("ROLE_USER")
 public class UserController extends BaseController {
 
     /**
      * The User service.
      */
-    @Autowired
-    UserService userService;
+    private final UserService userService;
 
     /**
-     * Authenticate user response entity.
+     * Instantiates a new User controller.
      *
-     * @param httpServletRequest  the http servlet request
-     * @param httpServletResponse the http servlet response
-     * @return the response entity
+     * @param userService the user service
      */
-    @RequestMapping(value = AUTHENTICATE, method = GET)
-    @ResponseBody
-    public ResponseEntity<RestResponseVO<Boolean>> authenticateUser(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        RestResponseVO<Boolean> restResponseVO = new RestResponseVO<>(FALSE, null, EMPTY);
-        try {
-            setRestResponseVO(restResponseVO, userService.authenticateUser(httpServletRequest.getHeader(X_AUTH_TOKEN)), null, getMessage(""));
-        } catch (Exception e) {
-            log.error("************** Error while authenticating user **************** \n");
-            e.getLocalizedMessage();
-        }
-        return new ResponseEntity<RestResponseVO<Boolean>>(restResponseVO, valueOf(httpServletResponse.getStatus()));
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    /**
-     * Login response entity.
-     *
-     * @param login               the login
-     * @param httpServletRequest  the http servlet request
-     * @param httpServletResponse the http servlet response
-     * @return the response entity
-     */
-    @RequestMapping(value = LOGIN, method = POST)
-    @ResponseBody
-    public ResponseEntity<RestResponseVO<String>> login(@RequestBody UserDTO login, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        RestResponseVO<String> restResponseVO = new RestResponseVO<>(null, null, EMPTY);
+    @RequestMapping(value = CURRENT_LOGGED_IN_USER, method = GET)
+    public ResponseEntity<UserDTO> getCurrentLoggedInUser(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         try {
-            setRestResponseVO(restResponseVO, userService.login(login), null, getMessage(""));
-        } catch (Exception e) {
-            log.error("************** Error while login user **************** \n");
-            e.getLocalizedMessage();
+            return new ResponseEntity<UserDTO>(convertFromUser(userService.getCurrentLoggedInUser()), valueOf(httpServletResponse.getStatus()));
+        } catch (HibernateException | IllegalArgumentException | NullPointerException e) {
+            log.error(format("[Request Interceptor Id {0}] {1}", httpServletRequest.getAttribute(CUSTOM_REQUEST_ID), getMessage("error.while.fetching.current.logged.in.user")));
+            log.error(getStackTrace(e));
+            return new ResponseEntity<UserDTO>(valueOf(httpServletResponse.getStatus()));
         }
-        return new ResponseEntity<RestResponseVO<String>>(restResponseVO, valueOf(httpServletResponse.getStatus()));
     }
 }
