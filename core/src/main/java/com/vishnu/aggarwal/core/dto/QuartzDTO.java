f229febb5b51839517b5f7891afaad234281bdf4
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-08-18 22:36:58 +0530

    added lombok annotations

--- core/src/main/java/com/vishnu/aggarwal/core/dto/QuartzDTO.java
+++ core/src/main/java/com/vishnu/aggarwal/core/dto/QuartzDTO.java
@@ -3,14 +3,16 @@ package com.vishnu.aggarwal.core.dto;
 import com.vishnu.aggarwal.core.co.APIJobDataCO;
 import com.vishnu.aggarwal.core.co.JobCO;
 import com.vishnu.aggarwal.core.enums.ScheduleType;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * The type Quartz dto.
  */
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class QuartzDTO {
     private JobCO job;
     private ScheduleType scheduleType;
