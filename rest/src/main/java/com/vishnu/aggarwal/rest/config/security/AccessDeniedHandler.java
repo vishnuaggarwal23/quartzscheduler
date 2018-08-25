01a62d8b3847692f8e94a5366d2054082a76b91c
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-08-26 00:19:38 +0530

    added exception message in response

--- rest/src/main/java/com/vishnu/aggarwal/rest/config/security/AccessDeniedHandler.java
+++ rest/src/main/java/com/vishnu/aggarwal/rest/config/security/AccessDeniedHandler.java
@@ -4,9 +4,9 @@ package com.vishnu.aggarwal.rest.config.security;
 Created by vishnu on 27/4/18 4:25 PM
 */
 
+import com.vishnu.aggarwal.core.config.BaseMessageResolver;
 import org.springframework.security.access.AccessDeniedException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,9 +17,16 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
  * The type Access denied handler.
  */
 public class AccessDeniedHandler implements org.springframework.security.web.access.AccessDeniedHandler {
+
+    private final BaseMessageResolver baseMessageResolver;
+
+    public AccessDeniedHandler(BaseMessageResolver baseMessageResolver) {
+        this.baseMessageResolver = baseMessageResolver;
+    }
+
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         response.setStatus(FORBIDDEN.value());
-        response.sendError(FORBIDDEN.value(), FORBIDDEN.getReasonPhrase());
+        response.sendError(FORBIDDEN.value(), baseMessageResolver.getMessage(accessDeniedException.getMessage(), accessDeniedException.getMessage()));
     }
 }
