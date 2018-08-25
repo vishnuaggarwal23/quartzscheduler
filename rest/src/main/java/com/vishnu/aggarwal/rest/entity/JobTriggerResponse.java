f8f0ae9727ef4fc13d0e7d4897356b62c8366919
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-08-26 00:26:59 +0530

    added validation annotations to fields. made jobGroup and triggerGroup an instance of User

--- rest/src/main/java/com/vishnu/aggarwal/rest/entity/JobTriggerResponse.java
+++ rest/src/main/java/com/vishnu/aggarwal/rest/entity/JobTriggerResponse.java
@@ -5,8 +5,13 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
+
+import static javax.persistence.FetchType.LAZY;
 
 /**
  * The type Job trigger response.
@@ -23,15 +28,39 @@ public class JobTriggerResponse extends BaseEntity<Long> implements Serializable
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String triggerKeyName;
-    private String triggerGroupName;
+
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    private User triggerGroupName;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String jobKeyName;
-    private String jobGroupName;
+
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    private User jobGroupName;
+
+    @NotNull
     private Integer responseCode;
+
+    @NotNull
     @Column(length = Integer.MAX_VALUE, columnDefinition = "LONGTEXT")
     private String responseHeader;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
     @Column(length = Integer.MAX_VALUE, columnDefinition = "LONGTEXT")
     private String responseBody;
+
+    @NotNull
     private Date fireTime;
 
 }
