07718bd5387756c065b173d4ade4108f9709c076
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-08-26 00:16:35 +0530

    added or updated JobType and ScheduleType enums

---
+++ core/src/main/java/com/vishnu/aggarwal/core/constants/JobType.java
@@ -0,0 +1,9 @@
+package com.vishnu.aggarwal.core.constants;
+
+/*
+Created by vishnu on 19/8/18 4:02 PM
+*/
+
+public class JobType {
+    public static final String API = com.vishnu.aggarwal.core.enums.JobType.API.getKey();
+}
---
+++ core/src/main/java/com/vishnu/aggarwal/core/constants/ScheduleType.java
@@ -0,0 +1,10 @@
+package com.vishnu.aggarwal.core.constants;
+
+/*
+Created by vishnu on 19/8/18 4:03 PM
+*/
+
+public class ScheduleType {
+    public static final String SIMPLE = com.vishnu.aggarwal.core.enums.ScheduleType.SIMPLE.getKey();
+    public static final String CRON = com.vishnu.aggarwal.core.enums.ScheduleType.CRON.getKey();
+}
