ea1dda6092ad4a76455909a5db9101220dc2b862
vishnu.aggarwal <vishnu.aggarwal@tothenew.com>
2018-08-26 00:20:06 +0530

    removed response flush

--- rest/src/main/java/com/vishnu/aggarwal/rest/config/security/LogoutSuccessHandler.java
+++ rest/src/main/java/com/vishnu/aggarwal/rest/config/security/LogoutSuccessHandler.java
@@ -2,10 +2,8 @@ package com.vishnu.aggarwal.rest.config.security;
 
 import org.springframework.security.core.Authentication;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 
@@ -17,8 +15,7 @@ Created by vishnu on 21/4/18 5:39 PM
 */
 public class LogoutSuccessHandler implements org.springframework.security.web.authentication.logout.LogoutSuccessHandler {
     @Override
-    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         response.setStatus(SC_OK);
-        response.getWriter().flush();
     }
 }
