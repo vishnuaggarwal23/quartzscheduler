f2dee1a798bf83c99c5fe0d5adadbfca0c63dca0
Vishnu Aggarwal <vishnu.aggarwal@tothenew.com>
2020-08-10 20:33:41 +0530

    Initial setup

---
+++ src/main/java/wrapper/quartz/scheduler/Application.java
@@ -0,0 +1,19 @@
+package wrapper.quartz.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Application.
+ */
+@SpringBootApplication
+public class Application {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
